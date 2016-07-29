45a9a3897e88bcff244b6fb3c57e1f3c0cef1331
Alex Coyle <abcoyle@gmail.com>
2016-07-29 20:43:21 +0100

    Use container_exec to setup stub (work in progress)

--- src/ansible_stub.bash
+++ src/ansible_stub.bash
@@ -1,8 +1,12 @@
 
 __stub_remote() {
-  local _format=$1 _exit=$2 _container  _command=$4 _args
+  local _format=$1 _exit=$2 _container _command=$4 _args
   IFS='|' read -a _container -r <<< $3
   shift 4
   _args=${@:-.*}
+  container_exec $_container mkdir -p /tmp/stub
+  # 2x ansible lineinfile to /tmp/stub/$_command
+  # 1x ansible lineinfile to ~/.bash_profile (PATH)
+  container_exec $_container chmod +x /tmp/stub/$_command
 }
 
